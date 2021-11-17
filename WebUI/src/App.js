4734ed7b8e8fc1063e7738a6cac8580076640468
1007297 <1007297@1007297T1082753>
2021-11-17 17:42:18 +0330

    React-router-dom & users page

--- WebUI/src/App.js
+++ WebUI/src/App.js
@@ -3,16 +3,21 @@ import SideBar from "./components/sidebar/SideBar";
 import TopBar from "./components/topbar/TopBar";
 import "./app.css";
 import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Switch, Route ,Routes} from "react-router-dom";
+import UserList from "./pages/userList/UserList";
 
 function App() {
   return (
-    <div>
+    <Router>
       <TopBar />
       <div className="container">
-        <SideBar/>
-        <Home/>
+        <SideBar />
+        <Routes>
+          <Route exact path="/" element={ <Home />}/>
+          <Route exact path="/users" element={ <UserList />}/>
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
